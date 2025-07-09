bac599fd4d41498f06247a408a7d489ba9140640
Arthur-Amorim-eng <arthur.silva.amorim@escola.pr.gov.br>
2025-07-09 07:27:41 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando o Brasil foi Descoberto?',
+    '1500'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas Copas Do Mundo o Brasil Tem?',
+    'Cinco'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a Capital De Espírito Santo?',
+    'Vitória'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como Se Diz Bom Dia Em Inglês?',
+    'Good Morning'
+)
